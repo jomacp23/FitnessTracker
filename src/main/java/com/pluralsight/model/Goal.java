3144613728aaa0bc6bbd45a18fbe5aca7688ee2c
josecruz <jose.m.cruz.perez@accenture.com>
2017-02-14 11:38:12 -0600

    FitnessTracker update

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -1,20 +1,71 @@
 package com.pluralsight.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import org.hibernate.validator.constraints.Range;
 
-
-
+@Entity
+@Table(name="goals")
+//@NamedQueries({
+//	@NamedQuery(
+//		name = Goal.FIND_GOAL_REPORTS, 
+//		query = "SELECT new com.pluralsight.model.GoalReport(g.minutes, e.minutes, e.activity) " 
+//			+ "FROM Goal g Exercise e WHERE g.id = e.goal.id"),
+//	@NamedQuery(
+//		name = Goal.FIND_ALL_GOALS, query = "SELECT g FROM Goal g")
+//})
 public class Goal {
+	
+	public static final String FIND_ALL_GOALS = "findAllGoal";
+	
+	public static final String FIND_GOAL_REPORTS = "findGoalReports";
+	
+	@Id
+	@GeneratedValue
+	@Column(name="GOAL_ID")
+	private Long id;
 
 	@Range(min = 1, max = 120)
+	@Column(name="MINUTES")
 	private int minutes;
+	
+	@OneToMany(mappedBy = "goal", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Exercise> exercises = new ArrayList<Exercise>();
 
+	public Long getId() {
+		return id;
+	}
+	
 	public int getMinutes() {
 		return minutes;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
+	}
+
+	public List<Exercise> getExercises() {
+		return exercises;
+	}
+
+	public void setExercises(List<Exercise> exercises) {
+		this.exercises = exercises;
 	}
 	
 }
